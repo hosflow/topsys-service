ad286998e907d6a9ebffdbdba6eb189ab8d6835b
André Topázio <andre.topazio@grupoatarde.com.br>
2021-01-29 20:28:30 -0300

    ...

--- src/main/java/br/com/topsys/service/exception/TSServiceException.java
+++ src/main/java/br/com/topsys/service/exception/TSServiceException.java
@@ -8,7 +8,6 @@ import org.springframework.dao.DuplicateKeyException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
