19537f1cd3d3a53c70ef61cca4c75151dc539a22
André Topázio <andre.topazio@gmail.com>
2021-02-25 11:09:40 -0300

    ...

--- src/main/java/br/com/topsys/service/jdbc/TSModelRowMapper.java
+++ src/main/java/br/com/topsys/service/jdbc/TSModelRowMapper.java
@@ -2,10 +2,12 @@ package br.com.topsys.service.jdbc;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Types;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.BeanWrapper;
 import org.springframework.beans.PropertyAccessorFactory;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.RowMapper;
 
 public class TSModelRowMapper<T> implements RowMapper<T> {
@@ -29,7 +31,13 @@ public class TSModelRowMapper<T> implements RowMapper<T> {
 		if (parametros != null) {
 			for (int x = 0; x < parametros.length; x++) {
 				if (wrapper.isWritableProperty(parametros[x])) {
-					wrapper.setPropertyValue(parametros[x], rs.getObject(x + 1));
+				
+				  if(Types.TIMESTAMP_WITH_TIMEZONE == rs.getMetaData().getColumnType(x + 1)){
+					  wrapper.setPropertyValue(parametros[x], rs.getTimestamp(x + 1));
+				  }else {
+					  wrapper.setPropertyValue(parametros[x], rs.getObject(x + 1));
+				  }
+					
 				}
 
 			}
@@ -38,5 +46,5 @@ public class TSModelRowMapper<T> implements RowMapper<T> {
 		return objeto;
 
 	}
-
+	
 }
