4a70a48b74fb0892153d9a532ab959c78c5cc70a
André Topázio <andre.topazio@gmail.com>
2021-07-30 20:50:53 -0300

    ...

--- src/main/java/br/com/topsys/service/jdbc/TSModelRowMapper.java
+++ src/main/java/br/com/topsys/service/jdbc/TSModelRowMapper.java
@@ -2,17 +2,12 @@ package br.com.topsys.service.jdbc;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Types;
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.time.OffsetDateTime;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.BeanWrapper;
 import org.springframework.beans.PropertyAccessorFactory;
 import org.springframework.jdbc.core.RowMapper;
 
-import br.com.topsys.base.exception.TSSystemException;
 import br.com.topsys.base.util.TSCryptoUtil;
 
 public class TSModelRowMapper<T> implements RowMapper<T> {
@@ -30,41 +25,26 @@ public class TSModelRowMapper<T> implements RowMapper<T> {
 	public T mapRow(ResultSet rs, int rowNum) throws SQLException {
 
 		T objeto = BeanUtils.instantiateClass(classe);
-		
+
 		try {
-			
+
 			objeto.getClass().getMethod("build").invoke(objeto);
-			
-		} catch (Exception e) {}
-		
+
+		} catch (Exception e) {
+		}
+
 		BeanWrapper wrapper = PropertyAccessorFactory.forBeanPropertyAccess(objeto);
 		wrapper.setAutoGrowNestedPaths(true);
 
 		if (parametros != null) {
 
 			for (int x = 0; x < parametros.length; x++) {
-
-				int column = rs.getMetaData().getColumnType(x + 1);
-
-				if (Types.TIMESTAMP_WITH_TIMEZONE == column || Types.TIMESTAMP == column) {
-
-					wrapper.setPropertyValue(parametros[x], rs.getObject(x + 1, OffsetDateTime.class));
-
-				} else if (Types.DATE == column) {
-
-					wrapper.setPropertyValue(parametros[x], rs.getObject(x + 1, LocalDate.class));
-
-				} else if (Types.TIME == column) {
-
-					wrapper.setPropertyValue(parametros[x], rs.getObject(x + 1, LocalTime.class));
-
+			
+				if (parametros[x].startsWith(DECRYPT)) {
+					wrapper.setPropertyValue(parametros[x].replace(DECRYPT, ""),
+							TSCryptoUtil.decrypt(rs.getObject(x + 1)));
 				} else {
-					if(parametros[x].startsWith(DECRYPT)) {
-						wrapper.setPropertyValue(parametros[x].replace(DECRYPT,""),TSCryptoUtil.decrypt(rs.getObject(x + 1)));
-					}else {
-						wrapper.setPropertyValue(parametros[x],rs.getObject(x + 1));
-					} 
-					
+					wrapper.setPropertyValue(parametros[x], rs.getObject(x + 1));
 				}
 
 			}
