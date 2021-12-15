36f4176b07ab3f45809c0e01ea969ede06f86c0e
André Topázio <andre.topazio@gmail.com>
2021-12-14 21:57:39 -0300

    ...

--- src/main/java/br/com/topsys/service/jdbc/TSJdbcTemplate.java
+++ src/main/java/br/com/topsys/service/jdbc/TSJdbcTemplate.java
@@ -1,5 +1,6 @@
 package br.com.topsys.service.jdbc;
 
+import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
 
 import org.springframework.stereotype.Component;
@@ -7,9 +8,15 @@ import org.springframework.stereotype.Component;
 @Component
 public class TSJdbcTemplate extends TSJdbcRepository {
 
+	private DataSource dataSource;
 	
-	public TSJdbcTemplate() {
-		
+	public TSJdbcTemplate(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
+	
+	@PostConstruct
+	public void init() {
+		super.getDAO().setDataSource(dataSource);
 	}
 	
 	public void setDataSource(DataSource dataSource) {
