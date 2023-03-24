28ab99715ce5299a10ad16d6332f8a29a7c3fa70
André Topázio <andre.topazio@gmail.com>
2023-03-23 23:12:51 -0300

    ...

--- src/main/java/br/com/topsys/service/exception/TSJpaServiceException.java
+++ src/main/java/br/com/topsys/service/exception/TSJpaServiceException.java
@@ -5,6 +5,10 @@ import java.util.Date;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -18,8 +22,6 @@ import jakarta.persistence.EntityNotFoundException;
 @RestControllerAdvice
 @Component
 public class TSJpaServiceException {
-
-	
 
 	@ExceptionHandler(DataIntegrityViolationException.class)
 	public ResponseEntity<Object> handleException(DataIntegrityViolationException ex) {
@@ -35,32 +37,60 @@ public class TSJpaServiceException {
 		return ResponseEntity.notFound().build();
 
 	}
-	
+
 	@ExceptionHandler(MethodArgumentNotValidException.class)
 	public ResponseEntity<Object> handleException400(MethodArgumentNotValidException ex) {
-		
+
 		var erros = ex.getFieldErrors();
-		
+
 		return ResponseEntity.badRequest().body(erros.stream().map(ErroValidacao::new).toList());
 
 	}
-	
+
 	@ExceptionHandler(MethodArgumentTypeMismatchException.class)
 	public ResponseEntity<Object> handleException400() {
-		
+
 		return ResponseEntity.badRequest().build();
 
 	}
 	
+
+	@ExceptionHandler(HttpMessageNotReadableException.class)
+	public ResponseEntity<Object> handleException400(HttpMessageNotReadableException ex) {
+		return ResponseEntity.badRequest().body(ex.getMessage());
+	}
 	
 
+	@ExceptionHandler(BadCredentialsException.class)
+	public ResponseEntity<Object> handleExceptionBadCredentials() {
+		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Credenciais inválidas");
+	}
 	
-	private record ErroValidacao(String field, String message) { 
-		public ErroValidacao(FieldError fieldError){
-			this(fieldError.getField(),fieldError.getDefaultMessage());
+
+	@ExceptionHandler(AuthenticationException.class)
+	public ResponseEntity<Object> handleExceptionAuthentication() {
+		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Falha na autenticação");
+	}
+	
+
+	@ExceptionHandler(AccessDeniedException.class)
+	public ResponseEntity<Object> handleExceptionAcessoNegado() {
+		return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Acesso negado");
+	}
+	
+
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<Object> handleException500(Exception ex) {
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro: " + ex.getLocalizedMessage());
+	}
+	
+	
+	
+
+	private record ErroValidacao(String field, String message) {
+		public ErroValidacao(FieldError fieldError) {
+			this(fieldError.getField(), fieldError.getDefaultMessage());
 		}
 	}
-
-
 
 }
