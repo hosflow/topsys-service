48c4c1bae68610538fb433997e0f216654cb875e
André Topázio <andre.topazio@gmail.com>
2025-01-11 12:49:52 -0300

    ...

--- src/main/java/br/com/topsys/service/exception/TSServiceException.java
+++ src/main/java/br/com/topsys/service/exception/TSServiceException.java
@@ -121,16 +121,5 @@ public class TSServiceException {
 
 	}
 
-	@ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Object> handleValidationExceptions(MethodArgumentNotValidException ex) {
-        Map<String, String> errors = new HashMap<>();
-    
-		ex.getBindingResult().getFieldErrors().forEach(error -> 
-            errors.put(error.getField(), error.getDefaultMessage())
-        );
-
-       return ResponseEntity.badRequest().body(errors);
-	}
-
-
 }
+ 
